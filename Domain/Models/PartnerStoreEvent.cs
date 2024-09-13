e9a56020222d11eb66fb9d6d1e741c7057f20434
ivancekikj <ivan.cekic10@gmail.com>
2024-09-13 23:07:33 +0200

    Integrated app with partner store

---
+++ Domain/Models/PartnerStoreEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class PartnerStoreEvent : BaseEntity
+    {
+        public string? EventName { get; set; }
+        public string? HostName { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<string>? Schedules { get; set; }
+    }
+}
---
+++ Web/Controllers/PartnerStoreController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Web.Controllers
+{
+    public class PartnerStoreController : Controller
+    {
+        public IActionResult Events()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "https://emsweb20240826182329.azurewebsites.net/api/Events/GetAllEvents";
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+            List<PartnerStoreEvent> partnerEvents = response.Content.ReadAsAsync<List<PartnerStoreEvent>>().Result;
+            return View(partnerEvents);
+        }
+
+        public IActionResult EventDetails(Guid id)
+        {
+            HttpClient client = new HttpClient();
+            string URL = $"https://emsweb20240826182329.azurewebsites.net/api/Events/{id}";
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+            PartnerStoreEvent partnerEvent = response.Content.ReadAsAsync<PartnerStoreEvent>().Result;
+            return View(partnerEvent);
+        }
+    }
+}
