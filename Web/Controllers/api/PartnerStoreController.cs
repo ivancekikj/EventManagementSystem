6430855b5dd7cfdb5384526dc8a99d1bed0f3bbd
ivancekikj <ivan.cekic10@gmail.com>
2024-09-13 18:59:32 +0200

    Added api controller for partner system

---
+++ Web/Controllers/api/PartnerStoreController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartnerStoreController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public PartnerStoreController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        // GET: api/PartnerStore/GetAllEvents
+        [HttpGet("[action]")]
+        public List<Event> GetAllEvents()
+        {
+            return _eventService.GetAll();
+        }
+
+        // GET api/PartnerStore/GetDetailsForEvent/{id}
+        [HttpGet("[action]/{id}")]
+        public Event GetDetailsForEvent(Guid id)
+        {
+            return _eventService.GetById(id);
+        }
+    }
+}
